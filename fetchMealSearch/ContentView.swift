dca4cf738f4a4740268ee0fe779df270bb27fa5e
Saksham <instant.saksham@gmail.com>
2023-06-21 15:43:32 -0700

    Initial Commit

---
+++ fetchMealSearch/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  fetchMealSearch
+//
+//  Created by user242726 on 6/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fetchMealSearch/fetchMealSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchMealSearchApp.swift
+//  fetchMealSearch
+//
+//  Created by user242726 on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct fetchMealSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
